6fc6073ef58bf50f87223e8d8aa6b304036e3944
Roland Schäuble <roland.schaeuble@gmail.com>
2025-05-12 08:18:51 +0200

    Enhanced user handling

--- CleanLogs.lua
+++ CleanLogs.lua
@@ -1,13 +1,20 @@
 #!/usr/bin/env lua
 
 local LOG_DIR = "/LOGS"
-local KEEP_LATEST_DATE = 0
-local KEEP_LATEST_LOG = 1
-local DELETE_ALL = 2
-local EXIT = 3
+local ACTION_KEEP_LATEST_DATE = 0
+local ACTION_KEEP_LATEST_LOG = 1
+local ACTION_DELETE_ALL = 2
 
-local menu_items = { "Keep latest date", "Keep latest log", "Delete all", "Exit" }
-local selection = 0
+local STATE_START = 0
+local STATE_SELECTING = 1
+local STATE_EXECUTING = 2
+local STATE_DONE = 3
+
+local menu_items = { "Keep latest date", "Keep latest log", "Delete all" }
+local action = 1
+
+local state = STATE_START
+local deletedFiles = 0
 
 local function isLogFile(fname)
     local l = string.len(fname)
@@ -82,27 +89,45 @@ local function keepFile(fname, keep_files)
     return false
 end
 
-local function cleanLogs(op)
+local function cleanLogs(action)
     local log_files = getLogFiles()
     local keep_files
-    if op == KEEP_LATEST_DATE then
+    local deleteCount = 0
+    if action == ACTION_KEEP_LATEST_DATE then
         keep_files = getLastDay(log_files)
-    elseif op == KEEP_LATEST_LOG then
+    elseif action == ACTION_KEEP_LATEST_LOG then
         keep_files = getLatestLogs(log_files)
-    elseif op == DELETE_ALL then
+    elseif action == ACTION_DELETE_ALL then
         keep_files = {}
     end
 
     for k,v in pairs(log_files) do
         if not keepFile(v, keep_files) then
             del(LOG_DIR .. "/" .. v)
+            deleteCount = deleteCount + 1
         end
     end
+    return deleteCount
 end
 
-local function drawHeader()
+local function updateGui()
+    lcd.clear()
     lcd.drawText(1, 0, "Clean Log Files", INVERS)
-    lcd.drawText(1, 10, "For each model:")
+
+    if state == STATE_DONE then
+        lcd.drawText(1,20, "Purged " .. deletedFiles .. " files")
+        lcd.drawText(1, 40, "Press RTN")
+    else
+        lcd.drawText(1, 10, "For each model:")
+        lcd.drawText(1, 40, "Long press Enter")
+        lcd.drawText(1, 50, "to exectue")
+
+        if state == STATE_START then
+            lcd.drawCombobox(1, 20, 120, menu_items, action, INVERS)
+        elseif state == STATE_SELECTING then
+            lcd.drawCombobox(1, 20, 120, menu_items, action, BLINK)
+        end
+    end
 end
 
 local function nextItem(list, current)
@@ -119,23 +144,54 @@ local function prevItem(list, current)
     return current
 end
 
-local function run(event)
-    if event == EVT_VIRTUAL_NEXT then
-        selection = nextItem(menu_items, selection)
-    elseif event == EVT_VIRTUAL_PREV then
-        selection = prevItem(menu_items, selection)
-    elseif event == EVT_VIRTUAL_ENTER then
-        if selection == EXIT then
-            return 1
-        else
-            cleanLogs(selection)
-       end
+local function handleStart(event)
+    updateGui()
+    if event == EVT_VIRTUAL_ENTER then
+        state = STATE_SELECTING
+    elseif event == EVT_VIRTUAL_ENTER_LONG then
+        state = STATE_EXECUTING
     end
-
-    lcd.clear()
-    drawHeader()
-    lcd.drawCombobox(1, 20, 120, menu_items, selection)
     return 0
+end
+
+local function handleSelecting(event)
+    updateGui()
+    if event == EVT_VIRTUAL_NEXT then
+        action = nextItem(menu_items, action)
+    elseif event == EVT_VIRTUAL_PREV then
+        action = prevItem(menu_items, action)
+    elseif event == EVT_VIRTUAL_ENTER then
+        state = STATE_START
+    end
+    return 0
+end
+
+local function handleExecuting(event)
+    deletedFiles = cleanLogs(action)
+    state = STATE_DONE
+    return 0
+end
+
+local function handleDone(event)
+    updateGui()
+    if event == EVT_EXIT_BREAK then
+        state = STATE_START
+    end
+    return 0
+end
+
+local function run(event)
+    local result = 0
+    if state == STATE_START then
+        result = handleStart(event)
+    elseif state == STATE_SELECTING then
+        result = handleSelecting(event)
+    elseif state == STATE_EXECUTING then
+        result = handleExecuting(event)
+    elseif state == STATE_DONE then
+        result = handleDone(event)
+    end
+    return result
 end
 
 return {
