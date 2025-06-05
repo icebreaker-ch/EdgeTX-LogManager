ac8fc267d619dd3e98aa9b288b19e835b21d783a
Roland Schäuble <roland.schaeuble@gmail.com>
2025-06-05 22:14:53 +0200

    Reworked data base and simplified

--- LogManager/logfiles.lua
+++ LogManager/logfiles.lua
@@ -17,41 +17,28 @@ function LogFiles.new()
     return self
 end
 
-function LogFiles:printModel(model)
-    for _,v in pairs(self.map[model]) do
-        print("    " .. v:getFileName())
-    end
-end
-
-function LogFiles:printMap()
-    for k,_ in pairs(self.map) do
-        print(k .. ":")
-        self:printModel(k)
-    end
-end
-
 function LogFiles:read()
-    self.fileCount = 0
+    self.logFiles = {}
+    self.models = {}
     self.modelCount = 0
-    self.map = {}
     for f in dir(LOG_DIR) do
         if LogFile.isLogFile(f) then
             local info = fstat(LOG_DIR .. "/" .. f)
             local logFile = LogFile.new(f, info)
             local modelName = logFile:getModelName()
-            if not self.map[modelName] then
-                self.map[modelName] = {}
+            append(self.logFiles, logFile)
+            if not self.models[modelName] then
+                self.models[modelName] = 1
                 self.modelCount = self.modelCount + 1
+            else
+                self.models[modelName] = self.models[modelName] + 1
             end
-            self.map[modelName][#self.map[modelName] + 1] = logFile
-            self.fileCount = self.fileCount + 1
         end
     end
-    -- self:printMap()
 end
 
 function LogFiles:delete(files)
-    for _,v in pairs(files) do
+    for _, v in pairs(files) do
         del(LOG_DIR .. "/" .. v:getFileName())
     end
     return #files
@@ -59,8 +46,8 @@ end
 
 function LogFiles:getModels()
     local models = {}
-    for k,_ in pairs(self.map) do
-        models[#models + 1] = k
+    for k, _ in pairs(self.models) do
+        append(models, k)
     end
     return models
 end
@@ -70,39 +57,23 @@ function LogFiles:getModelCount()
 end
 
 -- returns the file count for all models (model == nil) or specific model
-function LogFiles:getFileCount(--[[opt]]model)
+function LogFiles:getFileCount( --[[opt]] model)
     if model then
-        return #self.map[model]
-    else        
-        return self.fileCount
-    end
-end
-
--- returns the logs for all models (model == nil) or specific model
-function LogFiles:getLogs(--[[opt]]model)
-    local logs = {}
-    if model then
-        logs = self.map[model]
+        return self.models[model]
     else
-        for model in pairs(self.map) do
-            for _,log in pairs(self.map[model]) do
-                append(logs, log)
-            end
-        end
+        return #self.logFiles
     end
-    return logs
 end
 
 function LogFiles:getLastFlights()
-    local result = {}
-    for m,v in pairs(self.map) do
-        for _,l in pairs(self.map[m]) do
-            if not result[m] or l:getFileName() > result[m]:getFileName() then
-                result[m] = l
-            end
+    local map = {}
+    for _, log in pairs(self.logFiles) do
+        local modelName = log:getModelName()
+        if not map[modelName] or log:getModelName() > map[modelName]:getFileName() then
+            map[modelName] = log
         end
     end
-    return result
+    return map
 end
 
 function LogFiles:filter(filterSpec)
@@ -116,16 +87,15 @@ function LogFiles:filter(filterSpec)
 
 
     local result = {}
-    local logs = self:getLogs()
-    for _,l in pairs(logs) do
-        local fileName = l:getFileName()
+    for _, log in pairs(self.logFiles) do
+        local fileName = log:getFileName()
         local modelName, date, time = string.match(fileName, LOGFILE_PATTERN)
         if (not filterSpec.modelName or modelName == filterSpec.modelName) and
-        (not filterSpec.keepToday or date ~= today) and
-        (not filterSpec.size or filterSpec.size == l:getSize()) and
-        (not filterSpec.keepLastFlight or fileName ~= lastFlights[modelName]:getFileName()) and
-        (not filterSpec.keepLastDay or date ~= lastFlights[modelName]:getDate()) then
-            result[#result + 1] = l
+            (not filterSpec.keepToday or date ~= today) and
+            (not filterSpec.size or filterSpec.size == log:getSize()) and
+            (not filterSpec.keepLastFlight or fileName ~= lastFlights[modelName]:getFileName()) and
+            (not filterSpec.keepLastDay or date ~= lastFlights[modelName]:getDate()) then
+            append(result, log)
         end
     end
     return result
